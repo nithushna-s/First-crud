71b4492bb0cfaa1c6db7f4b2f4abf29f5534fe7f
Sivarasa Nithushna <sivarasanithushna@gmail.com>
2023-12-07 14:55:29 +0530

    first commit

---
+++ routes/routes.js
@@ -0,0 +1,78 @@
+
+
+const Model = require('../model/model');
+
+const express = require('express');
+
+const router = express.Router()
+
+module.exports = router;
+
+
+//Post Method//Post Method
+router.post('/post', async (req, res) => {
+    const data = new Model({
+        name: req.body.name,
+        age: req.body.age
+    })
+
+    try {
+        const dataToSave = await data.save();
+        res.status(200).json(dataToSave)
+    }
+    catch (error) {
+        res.status(400).json({message: error.message})
+    }
+})
+//Get all Method
+router.get('/getAll', async (req, res) => {
+    try{
+        const data = await Model.find();
+        res.json(data)
+    }
+    catch(error){
+        res.status(500).json({message: error.message})
+    }
+})
+//Get by ID Method
+router.get('/getOne/:id', (req, res) => {
+    res.send(req.params.id)
+})
+//Get by ID Method
+router.get('/getOne/:id', (req, res) => {
+    res.send('Get by ID API')
+})
+
+//Update by ID Method
+//Update by ID Method
+router.patch('/update/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const updatedData = req.body;
+        const options = { new: true };
+
+        const result = await Model.findByIdAndUpdate(
+            id, updatedData, options
+        )
+
+        res.send(result)
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+})
+
+
+
+//Delete by ID Method
+router.delete('/delete/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const data = await Model.findByIdAndDelete(id)
+        res.send(`Document with ${data.name} has been deleted..`)
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+})
+
